f228b2671f4ef11cb6481bfb554019a55aefff23
Aaron Kromer <aaron.kromer@gmail.com>
2015-04-13 17:36:33 -0400

    Ensure our RSpec dependency is met on load.
    We are dependent on rspec-core and rack-test. Ensure both of these
    dependencies are available to us before loading our code. If they are
    not, we may fail in unexpected ways.
    
    We are not attempting to `require` these dependencies. This is because
    we may be outside of the expected RSpec life cycle. We do not want to
    load it if this is the case, as that may cause strange behavior for them.

--- lib/rspec/api_doc.rb
+++ lib/rspec/api_doc.rb
@@ -1,3 +1,6 @@
+fail "rspec-core is not loaded" unless defined?(RSpec::Core)
+fail "rack-test is not loaded" unless defined?(Rack::Test)
+
 require "rspec/api_doc/version"
 
 module RSpec
--- spec/rspec/api_doc_spec.rb
+++ spec/rspec/api_doc_spec.rb
@@ -1,7 +1,21 @@
-require 'rspec/api_doc'
+RSpec.describe "RSpec::ApiDoc" do
+  it 'requires rspec-core to have been loaded' do
+    command = ['ruby', '-Ilib', '-e', 'require "rspec/api_doc"']
+    output = IO.popen(command, err: %i[ child out ])
 
-RSpec.describe RSpec::ApiDoc do
-  it 'has a version number' do
-    expect(RSpec::ApiDoc::VERSION).not_to be nil
+    expect(output.read).to include("rspec-core is not loaded (RuntimeError)")
+  end
+
+  it 'requires rack-test to have been loaded' do
+    command = [
+      "ruby",
+      "-Ibundle",
+      "-Ilib",
+      "-e",
+      'require "rspec/core"; require "rspec/api_doc"'
+    ]
+    output = IO.popen(command, err: %i[ child out ])
+
+    expect(output.read).to include("rack-test is not loaded (RuntimeError)")
   end
 end
