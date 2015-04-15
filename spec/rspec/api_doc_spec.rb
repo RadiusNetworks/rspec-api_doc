d81286bd1744f271b1908baa2d467e46871ee013
Aaron Kromer <aaron.kromer@gmail.com>
2015-04-15 12:47:14 -0400

    Remove checks on dependencies.
    Loading the gem fails because we cannot predict which order bundler will
    load the gems. We should not have these checks in place. Our gemspec
    will handle the dependency for us.

--- spec/rspec/api_doc_spec.rb
+++ spec/rspec/api_doc_spec.rb
@@ -1,21 +1,2 @@
 RSpec.describe "RSpec::ApiDoc" do
-  it 'requires rspec-core to have been loaded' do
-    command = ['ruby', '-Ilib', '-e', 'require "rspec/api_doc"']
-    output = IO.popen(command, err: %i[ child out ])
-
-    expect(output.read).to include("rspec-core is not loaded (RuntimeError)")
-  end
-
-  it 'requires rack-test to have been loaded' do
-    command = [
-      "ruby",
-      "-Ibundle",
-      "-Ilib",
-      "-e",
-      'require "rspec/core"; require "rspec/api_doc"',
-    ]
-    output = IO.popen(command, err: %i[ child out ])
-
-    expect(output.read).to include("rack-test is not loaded (RuntimeError)")
-  end
 end
